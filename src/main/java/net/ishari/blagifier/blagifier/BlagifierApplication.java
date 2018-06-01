0b17637fda28acd8797670e8999d9a2151a996c8
Ishari <mastoffp@gmail.com>
2018-06-02 00:57:37 +0200

    Initial commit

---
+++ src/main/java/net/ishari/blagifier/blagifier/BlagifierApplication.java
@@ -0,0 +1,12 @@
+package net.ishari.blagifier.blagifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlagifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlagifierApplication.class, args);
+    }
+}
