0976fb5a764475e602068523a08286a9c898edb1
Ishari <mastoffp@gmail.com>
2018-06-11 19:49:04 +0200

    Remove the unnecesary 'public' from findBySlug

--- src/main/java/net/ishari/blagifier/blagifier/article/ArticleRepository.java
+++ src/main/java/net/ishari/blagifier/blagifier/article/ArticleRepository.java
@@ -3,5 +3,5 @@ package net.ishari.blagifier.blagifier.article;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ArticleRepository extends CrudRepository<Article, Long> {
-    public Article findBySlug(String slug);
+    Article findBySlug(String slug);
 }
