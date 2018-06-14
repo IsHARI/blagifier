bc9a40fdd499e4f63818873bea7fede0897d6271
Ishari <mastoffp@gmail.com>
2018-06-14 17:21:29 +0200

    Enable getting all articles ordered by date

--- src/main/java/net/ishari/blagifier/blagifier/article/ArticleRepository.java
+++ src/main/java/net/ishari/blagifier/blagifier/article/ArticleRepository.java
@@ -2,6 +2,9 @@ package net.ishari.blagifier.blagifier.article;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
     Article findBySlug(String slug);
+    List<Article> findAllByOrderByDateCreatedDesc();
 }
